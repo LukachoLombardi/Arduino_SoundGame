2bc98f9102c903bd6c20ef490c317418b846cfa9
Lukacho <spamherrre@gmail.com>
2021-07-05 18:11:22 +0200

    kinda managed to remove the display delay

--- main/main.ino
+++ main/main.ino
@@ -16,11 +16,23 @@ Song songs[1];
 // NOTE_C7, 4, 2,
 // NOTE_D7, 4, 2
 
+void dispPrintAcc(String text, bool firstTime = false)
+{
+  if(firstTime)
+  {
+    lcd.clear();
+    lcd.home();
+    lcd.print("acc.:");
+  }
+  lcd.setCursor(0,1);
+  lcd.print(text + "%");
+}
+
 void resetLEDs(int minpin, int maxpin)
 {
-  for(int i = minpin; i < maxpin; i++)
+  for (int i = minpin; i < maxpin; i++)
   {
-    digitalWrite(i,LOW);
+    digitalWrite(i, LOW);
   }
 }
 
@@ -31,34 +43,34 @@ float play(Song song, bool useDisplay)
 }
 float play(int songNotes[], float songTimings[], int songDirections[], int songLength, int noteDelay, int tactLength, float maxPressDelay, bool useDisplay) //takes the Notes, the Timings, the Directions, the Length of the Song(IN NOTES, NOT IN MS!!!), the Length of a tact (IN MS, NOT IN NOTES!!!) and the max button press delay (Ill probably replace that with a multiplier for the absolute Length of a note).
 {
-
+  dispPrintAcc("//", true);
   logger.printline("starting song");
   float currentPressDelay = 0;
   float currentAcc = 0;
   float currentMaxPressDelay = 0;
   int pressCount = 0;
   float avgAcc = 0;
-  
-  for (int i = 0 ; i <= songLength-1 ; i++)
+
+  for (int i = 0 ; i <= songLength - 1 ; i++)
   {
 
     int currentNote = songNotes[i]; //get current frequency
     int currentDirection = songDirections[i]; //get current direction
     float currentTiming = songTimings[i]; //get current timing
 
-    if(tactLength * currentTiming == INFINITY || currentDirection > 2 || currentNote < 0)
+    if (tactLength * currentTiming == INFINITY || currentDirection > 2 || currentNote < 0)
     {
       logger.printline("something is wrong with the song input", "severe");
       break;
-    }    
+    }
 
     currentMaxPressDelay = (maxPressDelay * (tactLength * currentTiming));
-    
-    setLEDs(songDirections, songLength, i); //set the leds
-    
-    logger.printline((String)("playing tone with pitch, direction, Length: " + (String)(currentNote) + "," + (String)(currentDirection) + "," + (String)(tactLength*currentTiming)));
 
-    if(currentNote==0)
+    setLEDs(songDirections, songLength, i); //set the leds
+
+    logger.printline((String)("playing tone with pitch, direction, Length: " + (String)(currentNote) + "," + (String)(currentDirection) + "," + (String)(tactLength * currentTiming)));
+
+    if (currentNote == 0)
     {
       noTone(SPEAKERPIN);
     }
@@ -66,57 +78,63 @@ float play(int songNotes[], float songTimings[], int songDirections[], int songL
     {
       tone(SPEAKERPIN, currentNote); //playing the current sound
     }
-    if(currentDirection < 2)
+    if (currentDirection < 2)
     {
       currentPressDelay = measurePressDelay(currentDirection, currentMaxPressDelay);
       currentAcc = currentPressDelay / currentMaxPressDelay;
       avgAcc += currentAcc;
-      pressCount++; 
-      logger.printline("acc.: " + (String)(100-currentAcc*100) + "%");
-      if(useDisplay) {dispPrint("acc.: " + (String)(100-currentAcc*100) + "%");}
+      pressCount++;
+      logger.printline("acc.: " + (String)(100 - currentAcc * 100) + "%");
+      if (useDisplay) {
+        dispPrintAcc((String)(100 - currentAcc * 100));
+      }
       delay(tactLength * currentTiming - currentPressDelay); //waits until the note is over, depending on the time spent in measurePressDelay
     }
     else
     {
-      if(useDisplay){delay(tactLength * currentTiming + 0);}
-      else {delay(tactLength * currentTiming);} //make each delay a bit longer to accomodate for time lost to lcd printing, deactivated it because you cant really notice it
+      if (useDisplay) {
+        delay(tactLength * currentTiming + 0);
+      }
+      else {
+        delay(tactLength * currentTiming); //make each delay a bit longer to accomodate for time lost to lcd printing, deactivated it because you cant really notice it
+      }
     }
-    
+
     noTone(SPEAKERPIN);
-    delay(tactLength/noteDelay); //adds a really smol delay after each note
+    delay(tactLength / noteDelay); //adds a really smol delay after each note
   }
   noTone(SPEAKERPIN);
   resetLEDs(MINLEDPIN, MAXLEDPIN);
   logger.printline("end of song");
   avgAcc /= pressCount;
-  avgAcc = 100 - 100*avgAcc;
+  avgAcc = 100 - 100 * avgAcc;
   return avgAcc; //returns the average accuracy
 }
 
 void setLEDs (int songDirections[], int songLength, int initialIndex) //goes through every led, turns it off, and then turns it on if it finds a matching value at the according SongDirection index
 {
-  for(int i = 0; i <= MAXLEDPIN_0 - MINLEDPIN_0; i++)
+  for (int i = 0; i <= MAXLEDPIN_0 - MINLEDPIN_0; i++)
+  {
+    digitalWrite(MINLEDPIN_0 + i, LOW);
+    if (songLength > initialIndex + i) //checking left notes
     {
-      digitalWrite(MINLEDPIN_0 + i, LOW);
-      if(songLength > initialIndex + i) //checking left notes
+      if (songDirections[initialIndex + i] == 0)
       {
-          if(songDirections[initialIndex+i] == 0)
-          {
-           digitalWrite(MINLEDPIN_0 + i, HIGH);
-          }
-       }
+        digitalWrite(MINLEDPIN_0 + i, HIGH);
+      }
     }
-    for(int i = 0; i <= MAXLEDPIN_1 - MINLEDPIN_1; i++) //right notes
+  }
+  for (int i = 0; i <= MAXLEDPIN_1 - MINLEDPIN_1; i++) //right notes
+  {
+    digitalWrite(MINLEDPIN_1 + i, LOW);
+    if (songLength > initialIndex + i)
     {
-      digitalWrite(MINLEDPIN_1 + i, LOW);
-      if(songLength > initialIndex + i)
+      if (songDirections[initialIndex + i] == 1)
       {
-          if(songDirections[initialIndex+i] == 1)
-          {
-           digitalWrite(MINLEDPIN_1 + i, HIGH);
-          }
-       }
+        digitalWrite(MINLEDPIN_1 + i, HIGH);
+      }
     }
+  }
 }
 
 float measurePressDelay(int button, float maxPressDelay) //doesnt really "measure". Just waits for a specified max time and returns how long the player needed
@@ -124,31 +142,31 @@ float measurePressDelay(int button, float maxPressDelay) //doesnt really "measur
   long initialMillis = millis();
   int pin;
   int wrongPin;
-  switch(button)
+  switch (button)
   {
     case 0:
-    pin = BUTTONPIN_0;
-    wrongPin = BUTTONPIN_1;
-    break;
+      pin = BUTTONPIN_0;
+      wrongPin = BUTTONPIN_1;
+      break;
     case 1:
-    pin = BUTTONPIN_1;
-    wrongPin = BUTTONPIN_0;
-    break;
+      pin = BUTTONPIN_1;
+      wrongPin = BUTTONPIN_0;
+      break;
     default:
-    logger.printline("somethings wrong i can feel it", "warning");
+      logger.printline("somethings wrong i can feel it", "warning");
   }
-    while(millis() - initialMillis <= maxPressDelay)
+  while (millis() - initialMillis <= maxPressDelay)
+  {
+    if (digitalRead(wrongPin) == 0)
     {
-      if(digitalRead(wrongPin) == 0)
-      {
-        delay(maxPressDelay - (millis() - initialMillis));
-        break;
-      }
-      if(digitalRead(pin) == 0)
-      {
-       return int(millis()-initialMillis);
-      }
-   }
+      delay(maxPressDelay - (millis() - initialMillis));
+      break;
+    }
+    if (digitalRead(pin) == 0)
+    {
+      return int(millis() - initialMillis);
+    }
+  }
   return maxPressDelay;
 }
 
@@ -158,7 +176,7 @@ void dispPrint(String text) {
   lcd.print(text);
 }
 void dispPrint2(String text) {
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print(text);
 }
 
@@ -170,8 +188,11 @@ bool prompt(char *prompt, String yes, String no, int noPin, int yesPin) {
   lcd.print(yes);
   lcd.setCursor(0, 0);
   lcd.print(prompt);
-  while(digitalRead(noPin) == HIGH && digitalRead(yesPin) == HIGH){digitalRead(noPin); digitalRead(yesPin);}
-  if(digitalRead(noPin) == LOW)
+  while (digitalRead(noPin) == HIGH && digitalRead(yesPin) == HIGH) {
+    digitalRead(noPin);
+    digitalRead(yesPin);
+  }
+  if (digitalRead(noPin) == LOW)
   {
     logger.printline((String)prompt + " false", "debug");
     return false;
@@ -189,46 +210,50 @@ void setup() //some normal setup stuff (setting pins and serial initialization)
   lcd.backlight();
 
   Song::songCount = 0;
-  songs[0] = Song(noteDelay0,tactLength0,maxPressDelay0, songNotes0, songTimings0, songDirections0, sizeof(songNotes0), "Evangelion");
+  songs[0] = Song(noteDelay0, tactLength0, maxPressDelay0, songNotes0, songTimings0, songDirections0, sizeof(songNotes0), "Evangelion");
 
-  
+
   Serial.begin(9600);
   logger.init(&Serial);
   for (int i = BUTTONPIN_0 ; i <= BUTTONPIN_1 ; i++)
   {
-      pinMode(i, INPUT_PULLUP);
-      logger.printline("setting pin " + String(i) + " to input_pullup");
+    pinMode(i, INPUT_PULLUP);
+    logger.printline("setting pin " + String(i) + " to input_pullup");
   }
 
   for (int i = MINLEDPIN ; i <= MAXLEDPIN ; i++)
   {
-      pinMode(i, OUTPUT);
-      logger.printline("setting pin " + String(i) + " to output");
+    pinMode(i, OUTPUT);
+    logger.printline("setting pin " + String(i) + " to output");
   }
   //logger.printline((String)prompt("test", "no", "yes", BUTTONPIN_0, BUTTONPIN_1));
   //logger.printline((String)play(songs[0], true));
 }
 
-void loop() 
+void loop()
 {
   delay(750);
-    for(int i = 0; i < Song::songCount; i++)
+  for (int i = 0; i < Song::songCount; i++)
   {
     delay(750);
-    if(prompt(songs[i].songName, "next", "play", BUTTONPIN_0, BUTTONPIN_1))
+    if (prompt(songs[i].songName, "next", "play", BUTTONPIN_0, BUTTONPIN_1))
     {
       delay(750);
-      if(prompt("mode?", "try", "play", BUTTONPIN_0, BUTTONPIN_1))
+      if (prompt("mode?", "try", "play", BUTTONPIN_0, BUTTONPIN_1))
       {
         delay(500);
         float accuracy = play(songs[i], true);
         logger.printline("avg. accuracy: " + String(accuracy) + " percent");
-        dispPrint("avg. acc.:");   
+        dispPrint("avg. acc.:");
         dispPrint2((String)accuracy + "%");
         delay(2500);
         break;
       }
-      else {lcd.clear();play(songs[i], false); break;}
+      else {
+        lcd.clear();
+        play(songs[i], false);
+        break;
+      }
     }
     else
     {
